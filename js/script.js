90058f52fe328b91b16e439b5965d81aaab4e7d8
Thiago Fontanella <thiago.fontanella@partner.sdslasupport.com>
2023-08-22 11:07:13 -0300

    feature: add redirect to post and project cards

--- js/script.js
+++ js/script.js
@@ -15,12 +15,10 @@ await octokit.request("GET /user/repos", {
         headers: headers
     })
 .then((response) => {
-    console.log(response)
     const projectsParent = document.querySelector('.projects-resume')
     const { data } = response
 
     data.forEach(function (item) {
-        console.log(item)
         let name = item.name;
         let projectUrl = item.svn_url;
         let description = item.description;
@@ -29,15 +27,15 @@ await octokit.request("GET /user/repos", {
         let language = item.language;
 
         let productResume = `
-        <div class="project-resume">
-            <p class="project-resume-title"><ion-icon name="folder-outline"></ion-icon>${name}</p>
-            <p class="project-resume-description">${description}</p>
-            <div class="project-resume-footer">
-                <p class="project-resume-stars"><ion-icon name="star-outline"></ion-icon>${starCount}</p>
-                <p class="project-resume-pr"><ion-icon name="git-branch-outline"></ion-icon>${forksCount}</p>
-                <p class="project-resume-language">${language}</p>
-            </div>
-        </div>
+            <a href=${projectUrl} target="_blank" class="project-resume">
+                <p class="project-resume-title"><ion-icon name="folder-outline"></ion-icon>${name}</p>
+                <p class="project-resume-description">${description}</p>
+                <div class="project-resume-footer">
+                    <p class="project-resume-stars"><ion-icon name="star-outline"></ion-icon>${starCount}</p>
+                    <p class="project-resume-pr"><ion-icon name="git-branch-outline"></ion-icon>${forksCount}</p>
+                    <p class="project-resume-language">${language}</p>
+                </div>
+            </a>
         `
 
         projectsParent.insertAdjacentHTML(
